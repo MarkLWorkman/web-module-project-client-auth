ece24833393ba00d29015cb8753216460dea79b9
unknown <calanhaga@aim.com>
2021-04-10 17:02:53 -0400

    App.js complete

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import FriendsList from "./components/friends.js";
+import Login from "./components/login.js";
+import PrivateRoute from "./components/privateroute.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <PrivateRoute path="/friends" component={FriendsList} />
+      </Switch>
     </div>
   );
 }
